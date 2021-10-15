00abfae821cb4cd7f09c9540c4d59217f3c02ddd
waferris <waferris2@gmail.com>
2021-10-15 18:01:50 -0400

    add postMatchups response; add util functions for responses

---
+++ src/responses/postMatchups.js
@@ -0,0 +1,32 @@
+const _ = require("lodash");
+const { getBoxscores } = require("../helpers/boxscoreHelper");
+const addOrdinal = require("../utils/addOrdinal");
+const { postMessage } = require("../helpers/botHelper");
+
+function createMatchupsString(boxscores) {
+  return _.map(
+    boxscores,
+    ({ homeTeam, awayTeam }) =>
+      `${awayTeam.name} (${awayTeam.record.wins}-${
+        awayTeam.record.losses
+      }, ${addOrdinal(awayTeam.playoffSeed)}) vs ${homeTeam.name} (${
+        homeTeam.record.wins
+      }-${homeTeam.record.losses}, ${addOrdinal(homeTeam.playoffSeed)})`
+  );
+}
+
+const postMatchups = async (week) => {
+  try {
+    const boxscores = await getBoxscores(week);
+    const matchups = createMatchupsString(boxscores);
+    const message = "This Week's Matchups\n\n" + matchups.join("\n\n");
+    await postMessage(message);
+  } catch (error) {
+    console.error("Error posting matchups:\n", error);
+    throw Error("Error posting matchups.");
+  }
+};
+
+module.exports = {
+  postMatchups,
+};
---
+++ src/utils/addHeatScale.js
@@ -0,0 +1,22 @@
+module.exports = (streakType, streakLength) => {
+  switch (streakType) {
+    case "WIN":
+      if (streakLength > 1 && streakLength < 3) {
+        return "🔥";
+      } else if (streakLength >= 3 && streakLength < 5) {
+        return "🔥🔥";
+      } else if (streakLength >= 5) {
+        return "🔥🔥🔥";
+      }
+      return "";
+    case "LOSS":
+      if (streakLength > 1 && streakLength < 3) {
+        return "❄️";
+      } else if (streakLength >= 3 && streakLength < 5) {
+        return "❄️❄️";
+      } else if (streakLength >= 5) {
+        return "❄️❄️❄️";
+      }
+      return "";
+  }
+};
