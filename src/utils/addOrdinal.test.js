ea6909de3c3b1a0d8fb4b0ef495280bc819e64c7
Billy Ferris <waferris2@gmail.com>
2021-10-21 21:08:29 -0400

    refactor tests (#11)

--- src/utils/addOrdinal.test.js
+++ src/utils/addOrdinal.test.js
@@ -1,21 +1,17 @@
 const addOrdinal = require("./addOrdinal");
 
 describe("addOrdinal function", () => {
-  it("should return 1st", async () => {
-    expect(addOrdinal(1)).toEqual("1st");
-  });
-
-  it("should return 23rd", async () => {
-    expect(addOrdinal(23)).toEqual("23rd");
-  });
-
-  it("should return 11th", async () => {
-    expect(addOrdinal(11)).toEqual("11th");
-  });
-
-  it("should return 2nd", async () => {
-    expect(addOrdinal(2)).toEqual("2nd");
-  });
+  test.each([
+    { number: 1, numberWithOrdinal: "1st" },
+    { number: 23, numberWithOrdinal: "23rd" },
+    { number: 11, numberWithOrdinal: "11th" },
+    { number: 2, numberWithOrdinal: "2nd" },
+  ])(
+    "should return $number as $numberWithOrdinal",
+    ({ number, numberWithOrdinal }) => {
+      expect(addOrdinal(number)).toEqual(numberWithOrdinal);
+    }
+  );
 
   it("should throw an error if called without an argument", async () => {
     const expectedErrorMessage = "You must provide a number.";
